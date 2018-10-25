b1da4d42a6fef88b25148a0ef18e4c030297c5e5
Ganesh Rathinavel <ganeshrnet@live.com>
2018-10-25 21:32:55 +0530

    better electron window management

--- app/constants/env.js
+++ app/constants/env.js
@@ -2,3 +2,4 @@
 
 export const IS_DEV = process.env.NODE_ENV !== 'production';
 export const IS_PROD = process.env.NODE_ENV === 'production';
+export const DEBUG_PROD = process.env.DEBUG_PROD === 'true';
--- app/main.dev.js
+++ app/main.dev.js
@@ -11,19 +11,18 @@
  */
 import { app, BrowserWindow } from 'electron';
 import MenuBuilder from './menu';
+import { log } from './utils/log';
+import { IS_PROD, DEBUG_PROD, IS_DEV } from './constants/env';
 
 let mainWindow = null;
 const isSingleInstance = app.requestSingleInstanceLock();
 
-if (process.env.NODE_ENV === 'production') {
+if (IS_PROD) {
   const sourceMapSupport = require('source-map-support');
   sourceMapSupport.install();
 }
 
-if (
-  process.env.NODE_ENV === 'development' ||
-  process.env.DEBUG_PROD === 'true'
-) {
+if (IS_DEV || DEBUG_PROD) {
   require('electron-debug')();
   const path = require('path');
   const p = path.join(__dirname, '..', 'app', 'node_modules');
@@ -31,84 +30,106 @@ if (
 }
 
 const installExtensions = async () => {
-  const installer = require('electron-devtools-installer');
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
+  try {
+    const installer = require('electron-devtools-installer');
+    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+    const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
 
-  return Promise.all(
-    extensions.map(name => installer.default(installer[name], forceDownload))
-  ).catch(console.error);
+    return Promise.all(
+      extensions.map(name => installer.default(installer[name], forceDownload))
+    ).catch(console.error);
+  } catch (e) {
+    log.error(e, `main.dev -> installExtensions`);
+  }
 };
 
 if (!isSingleInstance) {
   app.quit();
 } else {
-  app.on('second-instance', (event, commandLine, workingDirectory) => {
-    if (mainWindow) {
-      if (mainWindow.isMinimized()) {
-        mainWindow.restore();
+  try {
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+      if (mainWindow) {
+        if (mainWindow.isMinimized()) {
+          mainWindow.restore();
+        }
+        mainWindow.focus();
       }
-      mainWindow.focus();
-    }
-  });
+    });
 
-  app.on('ready', () => {});
+    app.on('ready', () => {});
+  } catch (e) {
+    log.error(e, `main.dev -> second-instance`);
+  }
 }
 
 const createWindow = async () => {
-  if (
-    process.env.NODE_ENV === 'development' ||
-    process.env.DEBUG_PROD === 'true'
-  ) {
-    await installExtensions();
+  try {
+    if (
+      process.env.NODE_ENV === 'development' ||
+      process.env.DEBUG_PROD === 'true'
+    ) {
+      await installExtensions();
+    }
+
+    mainWindow = new BrowserWindow({
+      title: 'OpenMTP',
+      center: true,
+      show: false,
+      minWidth: 854,
+      minHeight: 640,
+      //frame: false
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/app.html`);
+
+    mainWindow.webContents.on('did-finish-load', () => {
+      if (!mainWindow) {
+        throw new Error('"mainWindow" is not defined');
+      }
+      if (process.env.START_MINIMIZED) {
+        mainWindow.minimize();
+      } else {
+        mainWindow.maximize();
+        mainWindow.show();
+        mainWindow.focus();
+      }
+    });
+
+    mainWindow.on('closed', () => {
+      mainWindow = null;
+    });
+
+    const menuBuilder = new MenuBuilder(mainWindow);
+    menuBuilder.buildMenu();
+  } catch (e) {
+    log.error(e, `main.dev -> createWindow`);
   }
-
-  mainWindow = new BrowserWindow({
-    title: 'OpenMTP',
-    center: true,
-    show: false,
-    minWidth: 854,
-    minHeight: 640
-    //frame: false
-  });
-
-  mainWindow.loadURL(`file://${__dirname}/app.html`);
-
-  // @TODO: Use 'ready-to-show' event
-  // https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
-  mainWindow.webContents.on('did-finish-load', () => {
-    if (!mainWindow) {
-      throw new Error('"mainWindow" is not defined');
-    }
-    if (process.env.START_MINIMIZED) {
-      mainWindow.minimize();
-    } else {
-      mainWindow.maximize();
-      mainWindow.show();
-      mainWindow.focus();
-    }
-  });
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-
-  const menuBuilder = new MenuBuilder(mainWindow);
-  menuBuilder.buildMenu();
 };
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
+  try {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  } catch (e) {
+    log.error(e, `main.dev -> window-all-closed`);
   }
 });
 
 app.on('ready', async () => {
-  await createWindow();
+  try {
+    await createWindow();
+  } catch (e) {
+    log.error(e, `main.dev -> ready`);
+  }
 });
 
 app.on('activate', async () => {
-  if (mainWindow === null) {
-    await createWindow();
+  try {
+    if (mainWindow === null) {
+      await createWindow();
+    }
+  } catch (e) {
+    log.error(e, `main.dev -> activate`);
   }
 });
