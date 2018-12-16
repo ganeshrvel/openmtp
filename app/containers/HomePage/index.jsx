8fb374686d0731bdc53fe6ae270b20461247b4a7
Ganesh Rathinavel <ganeshrnet@live.com>
2018-12-16 22:33:13 +0530

    todo updated

--- app/containers/HomePage/index.jsx
+++ app/containers/HomePage/index.jsx
@@ -20,31 +20,30 @@ class Home extends Component {
 
   async componentWillMount() {
     /*
-    todo: in the indefinite white screen which appears at the startup, as users to disconnect the phone
+    todo: in the indefinite white screen which appears at the startup, ask users to disconnect the phone
     todo: while copying multiple files the folder merge check fails
     todo: Add os name and version to the error log
     todo: privacy policy
     todo: crash report and analytics -> onboarding the first time users
     todo: display version number and release notes in updates
-    todo: opt out of auto updates check and analytics in settings
-    todo: opt out of auto updates check in the popup
-    todo: .env
-    todo: tags for github
+    todo: opt out of and analytics in settings
+    todo: opt out of auto updates check in the popup confirm box
     todo: in copy/paste, new folder, replace/merge, confirm, rename dialogues add the device name or sd card/int memory name
     todo: fix: drag files from local to mtp and then drop it back in the local. the merge dialog will appear
-    todo: look into pure components
-    todo: share the app link
-    todo: donate via paypal
-    todo: app store
-    todo: update to the binary
+    todo: update the binary
     todo: generate report should append ./mtp ls -v to the log before emailing
-    todo: Set target Babel
     todo: bump electron and mui versions
     todo: when you close the lid while update download is in process,
       "A javaScript error occured in the main process: Uncaught Exception: " error is thrown
       add process.on('uncaughtException', function (error) {
               // Handle the error
           }
+    todo: .env
+    todo: tags for github
+    todo: look into pure components
+    todo: share the app link
+    todo: donate via paypal
+    todo: app store
    */
   }
 
@@ -59,7 +58,10 @@ class Home extends Component {
               showMenu={true}
               deviceType={DEVICES_TYPE_CONST.local}
             />
-            <FileExplorer hideColList={[]} deviceType={DEVICES_TYPE_CONST.local} />
+            <FileExplorer
+              hideColList={[]}
+              deviceType={DEVICES_TYPE_CONST.local}
+            />
           </div>
           <div className={styles.splitPane}>
             <ToolbarAreaPane
