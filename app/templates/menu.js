56e4c00e20aad0a8cda1c6741d2f6068c85fefbf
Ganesh Rathinavel <ganeshrnet@live.com>
2018-12-25 13:25:11 +0530

    Invite a friend email body grammar updates

--- app/templates/menu.js
+++ app/templates/menu.js
@@ -5,6 +5,6 @@ import { APP_GITHUB_RELEASES_URL } from '../constants';
 
 const subject = `Android File Transfer for macOS - OpenMTP`;
 
-const body = `Looking for a good "Android File Transfer" App for macOS? Download "${APP_NAME}" the most advanced Android MTP File Transfer app for macOS from ${APP_GITHUB_RELEASES_URL} for free. It's Safe, OpenSourced and shall always stay Free!`;
+const body = `Looking for a good "Android File Transfer" app for macOS? Download "${APP_NAME}", The most advanced Android MTP File Transfer app for macOS from ${APP_GITHUB_RELEASES_URL} for free. It's Safe, OpenSourced and shall always stay Free!`;
 
 export const mailTo = `mailto:?Subject=${subject}&Body=${body}.`;
