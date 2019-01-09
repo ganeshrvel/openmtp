e749fc5e59ad70a226f9670998195b7006d70590
Ganesh Rathinavel <ganeshrnet@live.com>
2019-01-09 15:05:07 +0530

    HMR for docs webpack config

--- docs/webpack/webpack.config.dev.js
+++ docs/webpack/webpack.config.dev.js
@@ -4,10 +4,19 @@ const path = require('path');
 const PORT = 4644;
 const IS_DEV = process.env.NODE_ENV !== 'production';
 const IS_PROD = process.env.NODE_ENV === 'production';
+const buildPath = path.join(__dirname, '..', 'bundle');
 
 module.exports = {
   devServer: {
-    port: PORT
+    contentBase: buildPath,
+    port: PORT,
+    compress: true,
+    hot: true,
+    watchOptions: {
+      aggregateTimeout: 300,
+      ignored: /node_modules/,
+      poll: 100
+    }
   },
   plugins: [],
   module: {
