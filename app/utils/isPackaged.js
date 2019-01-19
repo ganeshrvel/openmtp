570e65948dad4bded4b76df8ebbb0b3b5fc5d26f
Ganesh Rathinavel <ganeshrnet@live.com>
2019-01-19 21:48:01 +0530

    eslint to js ispackaged optimizations

--- app/utils/isPackaged.js
+++ app/utils/isPackaged.js
@@ -1,6 +1,4 @@
 'use strict';
 
-import { IS_PROD } from '../constants/env';
-
 export const isPackaged =
-  IS_PROD && process.mainModule.filename.indexOf('app.asar') !== -1;
+  process.mainModule.filename.indexOf('app.asar') !== -1;
