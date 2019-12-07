f23c22b68cc6e1fffa8fb5fe406063ec8c80d4b8
Ganesh Rathinavel <ganeshrvel@outlook.com>
2019-12-07 22:54:09 +0530

    bumped electron version fixed isPackaged.js

--- app/utils/isPackaged.js
+++ app/utils/isPackaged.js
@@ -1,4 +1,12 @@
 'use strict';
 
-export const isPackaged =
-  process.mainModule.filename.indexOf('app.asar') !== -1;
+let _isPackaged = false;
+
+if (
+  process.mainModule &&
+  process.mainModule.filename.indexOf('app.asar') !== -1
+) {
+  _isPackaged = true;
+}
+
+export const isPackaged = _isPackaged;
