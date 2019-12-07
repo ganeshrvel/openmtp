b87aae68123613290b6611aee3c5b06db2cff659
Ganesh Rathinavel <ganeshrvel@outlook.com>
2019-12-08 01:47:35 +0530

    replaced isPackaged with electron-is-packaged

--- app/utils/isPackaged.js
+++ app/utils/isPackaged.js
@@ -1,14 +1,5 @@
 'use strict';
 
-let _isPackaged = false;
-
-if (
-  process.mainModule &&
-  process.mainModule.filename.indexOf('app.asar') !== -1
-) {
-  _isPackaged = true;
-} else if (process.argv.filter(a => a.indexOf('app.asar') !== -1).length > 1) {
-  _isPackaged = true;
-}
+import { isPackaged as _isPackaged } from 'electron-is-packaged';
 
 export const isPackaged = _isPackaged;
