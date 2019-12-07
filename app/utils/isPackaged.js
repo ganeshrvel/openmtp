49f740a00cbccd4061af801c290eed1311f3ed96
Ganesh Rathinavel <ganeshrvel@outlook.com>
2019-12-07 23:56:09 +0530

    fixed the renderer isPackage issue

--- app/utils/isPackaged.js
+++ app/utils/isPackaged.js
@@ -7,6 +7,8 @@ if (
   process.mainModule.filename.indexOf('app.asar') !== -1
 ) {
   _isPackaged = true;
+} else if (process.argv.filter(a => a.indexOf('app.asar') === -1).length > 1) {
+  _isPackaged = true;
 }
 
 export const isPackaged = _isPackaged;
