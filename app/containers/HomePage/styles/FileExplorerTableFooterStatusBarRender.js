f0dc17e983dc957e5dd4658b91e6a5586789bcb8
Ganesh Rathinavel <ganeshrvel@outlook.com>
2020-11-04 19:31:53 +0530

    Added device info to the statusbar

--- app/containers/HomePage/styles/FileExplorerTableFooterStatusBarRender.js
+++ app/containers/HomePage/styles/FileExplorerTableFooterStatusBarRender.js
@@ -7,6 +7,21 @@ export const styles = (theme) => ({
     textAlign: 'center',
   },
 
+  deviceTypeWrapper: {
+    fontWeight: '500',
+    color: theme.palette.secondary.main,
+    marginRight: 2,
+    marginLeft: 5,
+    fontSize: 12,
+    textTransform: 'capitalize',
+
+    [`& span`]: {
+      color: theme.palette.contrastPrimaryMainColor,
+      fontWeight: '600',
+      fontSize: 13,
+    },
+  },
+
   bodyWrapper: {
     fontWeight: '500',
     color: theme.palette.lightText1Color,
