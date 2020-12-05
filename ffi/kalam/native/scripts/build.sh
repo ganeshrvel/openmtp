3b505ec14fec2ff0a8e0b1a63c5528762893e3aa
Ganesh Rathinavel <ganeshrvel@outlook.com>
2020-12-06 00:50:13 +0530

    stripped abs path from the traces

--- ffi/kalam/native/scripts/build.sh
+++ ffi/kalam/native/scripts/build.sh
@@ -24,6 +24,9 @@ echo ""
 
 echo "building kalam..."
 
-(cd ./ffi/kalam/native && CGO_CFLAGS='-Wno-deprecated-declarations' \
-  go build -gcflags=-trimpath=$(go env GOPATH) -asmflags=-trimpath=$(go env GOPATH) \
-  -o ../../../build/mac/bin/kalam.dylib -buildmode=c-shared ./*.go)
+(
+  cd ./ffi/kalam/native && CGO_CFLAGS='-Wno-deprecated-declarations' \
+    go build \
+    -v -a -trimpath \
+    -o ../../../build/mac/bin/kalam.dylib -buildmode=c-shared ./*.go
+)
