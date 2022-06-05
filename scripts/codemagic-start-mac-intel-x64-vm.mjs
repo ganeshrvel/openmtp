755501cf1c3aefd747164d5bbe3a1d5986e55ac1
Ganesh Rathinavel <ganeshrvel@outlook.com>
2022-06-05 14:48:28 +0530

    codemagic.yaml updated

--- scripts/codemagic-start-mac-intel-x64-vm.mjs
+++ scripts/codemagic-start-mac-intel-x64-vm.mjs
@@ -4,7 +4,6 @@ import "zx/globals";
 import { packageDirectory } from "pkg-dir";
 import axiosPackage from "axios";
 import process from "process";
-import { spawn } from "child_process";
 
 require("dotenv").config();
 
@@ -14,7 +13,6 @@ $.shell = "/bin/zsh";
 await $`export LANG=en_US.UTF-8`;
 await $`export LC_ALL=en_US.UTF-8`;
 
-const PKG_ROOT_DIR = await packageDirectory();
 const CODEMAGIC_BASE_URL = `https://api.codemagic.io`;
 
 const axios = axiosPackage.create({
@@ -25,53 +23,17 @@ axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.headers.common["x-auth-token"] = process.env.CODEMAGIC_AUTH_TOKEN_ID;
 axios.defaults.timeout = 15000;
 
-console.log("=== process.env.CM_ARTIFACT_LINKS", process.env.CM_ARTIFACT_LINKS);
-console.log("=== typeof process.env.CM_ARTIFACT_LINKS", typeof process.env.CM_ARTIFACT_LINKS);
-
-// const cliArgs = basicArgs({
-//   name: 'codemagic-start-mac-intel-x64-vm',
-//   description: 'start codemagic mac intel x64 vm',
-//   errorOnUnknown: true,
-//   options: {
-//     'artifact-data': {
-//       type: String,
-//       description: 'base64 encoded artifact json data'
-//     },
-//   }
-// })
-//
-// console.log(cliArgs)
-
-
-const buildResponse = await axios.post("/builds", {
-  "appId": `${process.env.CODEMAGIC_APP_ID}`,
-  "workflowId": `${process.env.CODEMAGIC_INTEL_X64_WORKFLOW_ID}`,
-  "branch": `${process.env.CODEMAGIC_GIT_BRANCH}`,
-  "environment": {
-    "variables": {
-      "CM_ARTIFACT_LINKS_M1_ARM64": process.env.CM_ARTIFACT_LINKS
-    }
-  }
-});
-
-
-console.log("==== buildResponse", buildResponse);
-
-
-async function runSpawn(command, args) {
-  return new Promise((resolve, reject) => {
-    const buildSpawn = spawn(
-      command,
-      args,
-      { stdio: [0, 1, 2] }
-    );
-
-    buildSpawn.on("exit", (exitCode) => {
-      if (exitCode !== 0) {
-        return reject(exitCode);
+try {
+  await axios.post("/bui", {
+    "appId": `${process.env.CODEMAGIC_APP_ID}`,
+    "workflowId": `${process.env.CODEMAGIC_INTEL_X64_WORKFLOW_ID}`,
+    "branch": `${process.env.CODEMAGIC_GIT_BRANCH}`,
+    "environment": {
+      "variables": {
+        "CM_ARTIFACT_LINKS_M1_ARM64": process.env.CM_ARTIFACT_LINKS
       }
-
-      return resolve();
-    });
+    }
   });
+} catch (e) {
+  throw Error(e);
 }
