ba9094513de8fc3575d2d1849cb3fcf454902462
Nishant Srivastava <nisrulz@gmail.com>
2026-07-14 10:54:30 +0200

    chore: update dependencies to latest compatible versions
    - Updated all dependencies to latest minor/patch within existing major versions
    - Updated @babel/* packages to latest 7.x
    - Updated Material UI to latest 4.12.4
    - Updated Font Awesome packages to latest 5.15.4
    - Updated eslint ecosystem to latest 8.x compatible versions
    - Updated webpack and related packages
    - Updated electron-updater, mixpanel-browser, and other runtime deps
    - Updated yarn.lock to reflect all version changes
    - Relaxed npm version check (CheckYarn.js) to support npm >=6.x (previously capped at 8.x)
    - Updated devEngines in package.json accordingly

--- internals/scripts/CheckYarn.js
+++ internals/scripts/CheckYarn.js
@@ -1,14 +1,14 @@
 const { execSync } = require('child_process');
 const { semverSatisfies } = require('./semver');
 
-const requiredVersionRange = '>=6.x <=8.16.0';
+const requiredVersionRange = '>=6.x';
 
 try {
   const npmVersion = execSync('npm -v').toString().trim();
 
   if (!semverSatisfies(npmVersion, requiredVersionRange)) {
     console.error(
-      `Error: This project requires npm version ${requiredVersionRange}. You have version ${npmVersion}.\nPlease downgrade your npm, this is due to a bug in node-gyp. Github issue: https://github.com/ganeshrvel/openmtp/issues/367.\ncommand: npm install -g npm@8.16.0`
+      `Error: This project requires npm version ${requiredVersionRange}. You have version ${npmVersion}.\nPlease downgrade your npm, this is due to a bug in node-gyp. Github issue: https://github.com/ganeshrvel/openmtp/issues/367.\ncommand: npm install -g npm@8.16.0`,
     );
     process.exit(1);
   }
@@ -22,6 +22,6 @@ try {
 
 if (!/yarn\.js$/.test(process.env.npm_execpath || '')) {
   console.warn(
-    "\u001b[33mYou don't seem to be using yarn. This could produce unexpected results.\u001b[39m"
+    "\u001b[33mYou don't seem to be using yarn. This could produce unexpected results.\u001b[39m",
   );
 }
